fd20104b69dbeeedc32a92b068f31c62d47afb3a
XanaDevelops <danielgv.space@gmail.com>
2023-10-30 11:51:51 +0100

    strcmp

--- Practica1/my_lib.c
+++ Practica1/my_lib.c
@@ -30,25 +30,13 @@ size_t my_strlen(const char *str)
     return len;
 }
 
-int my_strcmp(const char *str1, const char *str2)
-{
+int my_strcmp(const char *str1, const char *str2){
     int i = 0;
-
-    while (str1[i] & str2[i])
-    {
-        char c1, c2;
-        c1 = str1[i];
-        c2 = str2[i];
-
-        if (c1 != c2)
-        {
-            return c1 - c2;
-        }
-
+    while(*(str1+i)!=0 && *(str2+i)!=0 && *(str1+i)==*(str2+i)){
         i++;
     }
 
-    return 0;
+    return *(str1+i)-*(str2+i);
 }
 
 char *my_strcpy(char *dest, const char *src)
