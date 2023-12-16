a17a1fd04aaaec8ff66225b65b3e77aba1435fcc
JoFeF08 <ferrioljosep08@gmail.com>
2023-12-16 04:00:42 +0100

    josep

--- practica2/prova.sh
+++
@@ -1,5 +0,0 @@
-echo "¡Hola! Bienvenido al mundo de las funciones en Bash."
-
-echo "¡Hola! Bienvenido al mundo de las funciones en GEFE."
-
-echo "¡Hola! Bienvenido al mundo de las funciones en feliz."
