69842d528b1fd400503bb511bab95f7aead92fee
bielperello <biel.perello1@estudiant.uib.cat>
2024-01-08 19:36:57 +0100

    Canvis

--- practica3/stack_counters.c
+++ practica3/stack_counters.c
@@ -58,29 +58,55 @@ void stack_init(const char *filename){
         exit(EXIT_FAILURE);
     }
 
+    fprintf(stdout, "Threads: %d, Iterations: %d\n" RESET, NUM_THREADS, N);
+
     // Verificar si la pila ya existe
     int file_exists = access(filename, F_OK);
     if (file_exists == -1){
         // Si no existe, crearla e inicializarla con punteros a 0
         stack = my_stack_init(NUM_THREADS);
 
-        for (int i = 0; i < NUM_THREADS; i++)
-        {
+        fprintf(stdout, "stack->size: %lu\n" RESET, sizeof(int));
+        fprintf(stdout, "initial stack length: %d\n" RESET, my_stack_len(stack));
+        fprintf(stdout, "initial stack content:\n" RESET);
+
+        // Imprimir contenido inicial de la pila
+        struct my_stack_node *current_node = stack->top;
+        while (current_node != NULL) {
+            fprintf(stdout, "%d\n", *(int *)current_node->data);
+            current_node = current_node->next;
+        }
+
+        fprintf(stdout, "stack content for treatment:\n");
+
+        for (int i = 0; i < NUM_THREADS; i++) {
             int *data = (int *)malloc(sizeof(int));
             *data = 0;
             my_stack_push(stack, data);
         }
 
+        // Imprimir nuevo tamaño de la pila
+        fprintf(stdout, "new stack length: %d\n", NUM_THREADS);
+
         // Guardar la pila en el fichero
         my_stack_write(stack, filename);
 
-        // Liberar memoria
-        int bytes = my_stack_purge(stack);
-        fprintf(stdout, "Released Bytes: %d \n", bytes);
+        my_stack_purge(stack);
     }
     else {
         // Si ya existe, cargar la pila desde el fichero en la variable global
         stack = my_stack_read(filename);
+
+        // Imprimir información sobre la pila original si existe
+        fprintf(stdout, "original stack content:\n");
+        struct my_stack_node *current_node = stack->top;
+        while (current_node != NULL) {
+            fprintf(stdout, "%d\n", *(int *)current_node->data);
+            current_node = current_node->next;
+        }
+
+        // Imprimir la longitud de la pila original
+        fprintf(stdout, "original stack length: %d\n", my_stack_len(stack));
 
         // Comprobar si la pila tiene menos de 10 elementos o ninguno
         int current_size = my_stack_len(stack);
@@ -119,17 +145,33 @@ void *worker(void *ptr){
     return 0;
 }
 
-void stack_end() {
+void stack_end(const char *filename) {
     // Esperar a que todos los hilos terminen
     for (int i = 0; i < NUM_THREADS; i++) {
         pthread_join(pthreads[i], NULL);
     }
 
+    fprintf(stdout, "stack content after threads iterations:\n");
+
+    // Imprimir el contenido de la pila después de las iteraciones de los hilos
+    struct my_stack_node *current_node = stack->top;
+    while (current_node != NULL) {
+        fprintf(stdout, "%d\n", *(int *)current_node->data);
+        current_node = current_node->next;
+    }
+
+    // Imprimir la longitud de la pila
+    fprintf(stdout, "stack length: %d\n", my_stack_len(stack));
+
     // Guardar la pila en un fichero
-    my_stack_write(stack, "output_stack.txt");
+    my_stack_write(stack, filename);
+
+    // Imprimir la información sobre los elementos escritos en el fichero
+    fprintf(stdout, "Written elements from stack to file: %d\n", my_stack_len(stack));
 
     // Liberar espacio de la pila
-    my_stack_purge(stack);
+    int bytes = my_stack_purge(stack);
+    fprintf(stdout, "Released bytes: %d\n", bytes);
 
     // Finalizar los hilos
     pthread_exit(NULL);
