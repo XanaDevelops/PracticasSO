f02f9c4eb5df33b74d665be93971c250f8946b7f
bielperello <biel.perello1@estudiant.uib.cat>
2024-02-15 13:02:11 +0100

    jofefo brut

--- practicaSF/bloques.c
+++ practicaSF/bloques.c
@@ -1,1 +1,10 @@
 #include "bloques.h"
+
+static int descriptor = 0;
+
+int bmount(const char *camino) {
+    descriptor = open(camino, O_RDWR | O_CREAT, 0666);
+
+    if(descriptor == -1) {}
+    
+}
