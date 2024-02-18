e9ea8e1dcbcde104644204e1da310b11b3282fa2
bielperello <biel.perello1@estudiant.uib.cat>
2024-02-18 12:14:32 +0100

    funció bmount i bwrite

--- practicaSF/bloques.c
+++ practicaSF/bloques.c
@@ -3,8 +3,46 @@
 static int descriptor = 0;
 
 int bmount(const char *camino) {
+    // Abrir el fichero como lectura y escritura, creándolo si no existe
     descriptor = open(camino, O_RDWR | O_CREAT, 0666);
 
-    if(descriptor == -1) {}
-    
+    // Comprobar si ha habido un error al abrir el fichero
+    if(descriptor == -1) {
+        // Gestión de error
+        perror(RED "Error");
+        printf(RESET);
+        // Devolver FALLO
+        return FALLO;
+    }
+
+    // Devolver el descriptor del fichero
+    return descriptor;
+}
+
+int bwrite(unsigned int nbloque, const void *buf) {
+    // Calcular el desplazamiento del dispositivo virtual
+    off_t desplazamiento = nbloque*BLOCKSIZE;
+
+    // Mover el puntero al desplazamiento calculado
+    off_t puntero = lseek(descriptor, desplazamiento, SEEK_SET);
+
+    if(puntero == -1) {
+        // Gestión de error
+        perror(RED "Error");
+        printf(RESET);
+        return FALLO;
+    }
+
+    // Escribir el contenido del buffer en el bloque especificado
+    size_t numBytes = write(descriptor, buf, BLOCKSIZE);
+
+    if(numBytes == -1) {
+        // Gestión de error
+        perror(RED "Error");
+        printf(RESET);
+        return FALLO;
+    }
+
+    // Devolver el número de bytes escrito en el fichero
+    return numBytes;
 }
