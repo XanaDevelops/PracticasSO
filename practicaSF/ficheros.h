df0b734c67573d82a4cf623d017db074e2978caf
XanaDevelops <danielgv.space@gmail.com>
2024-03-31 22:31:11 +0200

    actualizado makefile, fixbug ficheros.h

--- practicaSF/ficheros.h
+++ practicaSF/ficheros.h
@@ -1,3 +1,5 @@
+#include "ficheros_basico.h"
+
 // Nivel 5
 int mi_write_f(unsigned int ninodo, const void *buf_original, unsigned int offset, unsigned int nbytes);
 int mi_read_f(unsigned int nionodo, void *buf_original, unsigned int offset, unsigned int nbytes);
