4ccc14ed1ffa5c8aa5916bc7c12f10154567717d
JoFeF08 <ferrioljosep08@gmail.com>
2024-04-04 12:27:39 +0200

    escribir

--- practicaSF/escribir.c
+++ practicaSF/escribir.c
@@ -8,7 +8,7 @@ void errorExit();
 
 int main(int argc, char **argv)
 {
-    
+
     long int offsets[] = {9000, 209000, 30725000, 409605000, 480000000};
     char *buffer_original = argv[2];
     int buffer_size = strlen(buffer_original);
@@ -21,76 +21,57 @@ int main(int argc, char **argv)
         return FALLO;
     }
 
-     // montamos disco
+    // montamos disco
     if (bmount(*(argv + 1)) == FALLO)
     {
         return FALLO;
     }
+    int inodos_varios = 1;
+    int inodoReservado;
 
-    // comprobamos numero bloques
-    if (atoi(*(argv + 3)) == 0)
+    for (int i = 0; i < (sizeof(offsets) / sizeof(offsets[0])); i++)
     {
-        int inodoReservado = reservar_inodo('f', 6); // mirar si van bien
-
-        if (inodoReservado == FALLO)
+        if (inodos_varios == 1)
         {
-            fprintf(stderr, RED "ERROR: reservar_inodo(): no se ha podido reservar inodo\n" RESET);
-            errorExit();
-        }
-        for (int i = 0; i < (sizeof(offsets) / sizeof(offsets[0])); i++)
-        {
-            struct STAT estado;
-            mi_stat_f(inodoReservado, &estado);
-            print_estado(&estado);
 
-            mi_write_f(inodoReservado, &buffer_original, offsets[i], buffer_size);
-
-            mi_stat_f(inodoReservado, &estado);
-            print_estado(&estado);
-
-            memset(&buffer_original, '\0', buffer_size);
-            mi_read_f(inodoReservado, &buffer_original, offsets[i], buffer_size);
-            write(1, buffer_original, buffer_size);
-        }
-    }
-    else if (atoi(*(argv + 3)) == 1)
-    {
-        for (int i = 0; i < (sizeof(offsets) / sizeof(offsets[0])); i++)
-        {
             int inodoReservado = reservar_inodo('f', 6); // mirar si van bien
 
             if (inodoReservado == FALLO)
             {
-                fprintf(stderr, RED "ERROR: leer_sf(): no se ha podido reservar inodo\n" RESET);
+                fprintf(stderr, RED "ERROR: escribir.c: no se ha podido reservar inodo\n" RESET);
                 return FALLO;
             }
-
-            struct STAT estado;
-            mi_stat_f(inodoReservado, &estado);
-            print_estado(&estado);
-
-            mi_write_f(inodoReservado, &buffer_original, offsets[i], buffer_size);
-
-            mi_stat_f(inodoReservado, &estado);
-            print_estado(&estado);
-
-            memset(&buffer_original, '\0', buffer_size);
-            mi_read_f(inodoReservado, &buffer_original, offsets[i], buffer_size);
-            write(1, buffer_original, buffer_size);
+            inodos_varios = atoi(*(argv + 3));
         }
-    }
-    else
-    {
-        fprintf(stderr, RED "ARGUMENTOS INVALIDOS --> <diferentes_inodos> = 1 || 0 \n" RESET);
-         errorExit();
+        else if(inodos_varios != 0)
+        {
+            fprintf(stderr, RED "ARGUMENTOS INVALIDOS --> <diferentes_inodos> = 1 || 0 \n" RESET);
+            errorExit();
+        }
+
+        printf("Nº inodo reservado: %d\n", inodoReservado);
+        printf("Offset: %ld\n", offsets[i]);
+
+        struct STAT estado;
+        mi_stat_f(inodoReservado, &estado);
+        print_estado(&estado);
+
+        int bytes = mi_write_f(inodoReservado, &buffer_original, offsets[i], buffer_size);
+        printf("Bytes escritos: %d\n", bytes);
+
+        mi_stat_f(inodoReservado, &estado);
+        print_estado(&estado);
+
+        memset(&buffer_original, '\0', buffer_size);
+        mi_read_f(inodoReservado, &buffer_original, offsets[i], buffer_size);
+        write(1, buffer_original, buffer_size);
     }
 
-     // desmontamos disco
+    // desmontamos disco
     if (bumount())
     {
         return FALLO;
     }
-
 }
 
 void errorExit()
@@ -99,16 +80,11 @@ void errorExit()
     exit(FALLO);
 }
 
-
 void print_estado(struct STAT *estado)
 {
     printf("\n\nESTATS\n");
     printf("Tipo: %c\n", estado->tipo);
-    printf("Permisos: %c\n", estado->permisos);
-
-    printf("Fecha y hora del último acceso a datos: %s", ctime(&(estado->atime)));
-    printf("Fecha y hora de la última modificación de datos: %s", ctime(&(estado->mtime)));
-    printf("Fecha y hora de la última modificación del inodo: %s", ctime(&(estado->ctime)));
+    printf("Permisos: %d\n", estado->permisos);
 
     printf("Cantidad de enlaces de entradas en directorio: %u\n", estado->nlinks);
     printf("Tamaño en bytes lógicos (EOF): %u\n", estado->tamEnBytesLog);
