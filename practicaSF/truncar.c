97c80c55623880d0c9a9c39c86039a2c6411b8f7
JoFeF08 <ferrioljosep08@gmail.com>
2024-04-08 20:45:18 +0200

    truncar

--- practicaSF/truncar.c
+++ practicaSF/truncar.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include "ficheros.h" //fer include del més nou, per al nivell6, "ficheros.h"
 
+int imprimir_estado(struct STAT *estado);
+
 int main(int argc, char **argv)
 {
     // Comprobamos argumentos de consola
@@ -31,10 +33,32 @@ int main(int argc, char **argv)
         mi_truncar_f(ninodo, nbytes);
     }
 
+    struct STAT estado;
+    mi_stat_f(ninodo, &estado);
+
+    printf("\nDATOS INODO: %d\n", ninodo);
+    imprimir_estado(&estado);
+
+    
+
     // desmontamos disco
     if (bumount() == FALLO)
     {
         return FALLO;
     }
+    return EXITO;
+}
+
+int imprimir_estado(struct STAT *estado)
+{
+    fprintf(stdout, "tipo: %c\n", estado->tipo);
+    fprintf(stdout, "permisos: %d\n", estado->permisos);
+    fprintf(stdout, "atime: %s", ctime(&estado->atime));
+    fprintf(stdout, "ctime: %s", ctime(&estado->ctime));
+    fprintf(stdout, "mtime: %s", ctime(&estado->mtime));
+    fprintf(stdout, "nlinks: %d\n", estado->nlinks);
+    fprintf(stdout, "tamEnBytesLog: %d\n", estado->tamEnBytesLog);
+    fprintf(stdout, "numBloquesOcupados: %d\n", estado->numBloquesOcupados);
+
     return EXITO;
 }
