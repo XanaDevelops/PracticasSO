1767b6b0c6fbddc8b561c1ad44afd3cba6aafd4c
JoFeF08 <ferrioljosep08@gmail.com>
2024-05-08 11:20:12 +0200

    mi_escribir

--- practicaSF/mi_escribir.c
+++ practicaSF/mi_escribir.c
@@ -6,17 +6,17 @@ Perelló Perelló, Biel*/
 #include <stdio.h>
 #include <stdlib.h>
 #include "directorios.h"
-int  exitError();
+int exitError();
 
-int exitError(){
-// Desmontar el disco antes de salir
-        if (bumount() == FALLO)
-        {
-            return FALLO;
-        }
-
+int exitError()
+{
+    // Desmontar el disco antes de salir
+    if (bumount() == FALLO)
+    {
         return FALLO;
+    }
 
+    return FALLO;
 }
 
 int main(int argc, char **argv)
@@ -35,24 +35,44 @@ int main(int argc, char **argv)
     }
 
     char *camino = argv[2];
-    char *buffer_original = argv[3];
-    int offset = atoi(*(argv + 4));
-    int buffer_size = strlen(buffer_original);
+    int longitud_texto = 0;
+
+    int n_palabras = 0;
+    while (n_palabras < argc - 4)
+    {
+        longitud_texto += strlen(argv[n_palabras + 3]);
+        n_palabras++;
+    }
+    int n_espacios = n_palabras - 1;
+    longitud_texto += n_espacios;
+
+    char buffer_original[longitud_texto];
+    strcpy(buffer_original, argv[3]);
+    for (int i = 1; i < n_palabras; i++)
+    {
+        strcat(buffer_original, " ");
+        strcat(buffer_original, argv[i + 3]);
+    }
+
+    int offset = atoi(*(argv + argc - 1));
+    printf("longitud_texto %d \n", longitud_texto);
+    printf("offset %d \n", offset);
+ printf("buffer_original %s \n", buffer_original);
     int bytes;
 
-    if (*(camino + buffer_size - 1) != '/')
+    if (*(camino + longitud_texto - 1) != '/')
     {
         // Llamada a mi_write de la capa de directorios
-        bytes = mi_write(camino, buffer_original, offset, buffer_size);
+        bytes = mi_write(camino, buffer_original, offset, longitud_texto);
     }
     else
     {
         fprintf(stderr, RED "RUTA INVÁLIDA: La ruta para mi_write leer fichero no debe terminar con '/'.\n" RESET);
-        printf("Bytes Leidos: %d\n", 0);
+        printf("Bytes Escritos: %d\n", 0);
         return exitError();
     }
 
-    printf("Bytes Leidos: %d\n", bytes);
+    printf("Bytes Escritos: %d\n", bytes);
 
     // desmontamos disco
     if (bumount() == FALLO)
