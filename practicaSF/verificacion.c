06297729e7fdbb79f5b862d244bb51ebe30b5b8e
JoFeF08 <ferrioljosep08@gmail.com>
2024-05-23 10:16:24 +0200

    verificacion

--- practicaSF/verificacion.c
+++ practicaSF/verificacion.c
@@ -3,6 +3,7 @@ García Vázquez, Daniel
 Perelló Perelló, Biel*/
 
 #include "verificacion.h"
+#define NREGISTROS 256
 // Mejora 2,
 
 int exitError()
@@ -40,16 +41,14 @@ int main(int argc, char **argv)
     }
     int numentradas = s_stat.tamEnBytesLog / sizeof(struct entrada);
 
-    if (numentradas != NUMPROCESOS)
-    {
-        return exitError();
-    }
+    /*  if (numentradas != NUMPROCESOS)
+      {
+          return  exitError();}*/
 
     // Crear el fichero "informe.txt" dentro del directorio de simulación.
     char informe[100];
     strcpy(informe, argv[2]);
     strcat(informe, "informe.txt");
-
     if (mi_creat(informe, 6) == FALLO)
     {
         return exitError();
@@ -63,9 +62,14 @@ int main(int argc, char **argv)
     }
 
     struct INFORMACION buff_info[numentradas];
+    struct REGISTRO buff_reg[NREGISTROS];
+    memset(buff_reg, '\0', sizeof(buff_reg));
+    memset(buff_info, '\0', sizeof(buff_info));
+
     for (int i = 0; i < numentradas; i++)
     {
         char *inici = strchr(buff_entradas[i].nombre, '_');
+
         if (inici)
         {
             buff_info[i].pid = atoi(inici + 1);
@@ -76,5 +80,65 @@ int main(int argc, char **argv)
             buff_info[i].pid = FALLO;
             return exitError();
         }
+
+        char prueba[100];
+
+        strcpy(prueba, argv[2]);
+        strcat(prueba, buff_entradas[i].nombre);
+        strcat(prueba, "/prueba.dat");
+
+        int escriturasLeidas = 0;
+        int leidosT = 0;
+        int leidos = mi_read(informe, buff_reg, leidosT, sizeof(buff_reg));
+        leidosT += leidos;
+
+        while (leidos != 0)
+        {
+            int ulitmoreg = leidos / sizeof(struct REGISTRO);
+
+            for (int j = 0; j < ulitmoreg; j++)
+            {
+                if (buff_reg[j].pid == buff_info[i].pid)
+                {
+                    if (escriturasLeidas == 0)
+                    {
+                        // Inicializar los registros significativos con los datos de esa escritura.
+                        buff_info[i].MenorPosicion = buff_reg[j];
+                        buff_info[i].PrimeraEscritura = buff_reg[j];
+                        buff_info[i].UltimaEscritura = buff_reg[j];
+                    }
+                    else
+                    {
+                        // Comparar nº de escritura (para obtener primera y última) y actualizarlas si es preciso
+                        if (buff_reg[j].nEscritura < buff_info[i].PrimeraEscritura)
+                        {
+                            buff_info[i].PrimeraEscritura = buff_reg[j];
+                        }
+                        if (buff_reg[j].nEscritura > buff_info[i].UltimaEscritura)
+                        {
+                            buff_info[i].UltimaEscritura = buff_reg[j];
+                        }
+                    }
+                    // Incrementar contador escrituras validadas.
+                    escriturasLeidas++;
+                }
+            }
+            struct REGISTRO ultimo;
+            ultimo = buff_reg[ulitmoreg - 1];
+
+            memset(buff_reg, '\0', sizeof(buff_reg));
+            leidos = mi_read(informe, buff_reg, leidosT, sizeof(buff_reg));
+            leidosT += leidos;
+        }
+        // Obtener la escritura de la última posición.
+        buff_info[i].MayorPosicion = ultimo;
+        buff_info[i].nEscrituras= escriturasLeidas;
+    }
+    mi_write(informe, buff_info,0,sizeof(buff_info));
+
+    // Desmontar disco
+    if (bumount() == FALLO)
+    {
+        return FALLO;
     }
 }
