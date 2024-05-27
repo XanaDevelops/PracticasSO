4b97a351830a66628318d6b03ba12a83ef68cf69
XanaDevelops <danielgv.space@gmail.com>
2024-05-27 17:33:30 +0200

    test12 fix

--- practicaSF/test12.sh
+++ practicaSF/test12.sh
@@ -16,7 +16,7 @@ result=$(./mi_ls disco /)
 trimmed_result=$(echo "$result" | sed 's/^[ \t]*//;s/[ \t]*$//')
 echo /$trimmed_result/
 echo copia y ejecuta esto:
-echo ./verificacion disco /$(echo "$trimmed_result")/ && ./mi_cat disco /$(echo "$trimmed_result")/informe.txt
+echo ./verificacion disco /$(echo "$trimmed_result")/ "&&" ./mi_cat disco /$(echo "$trimmed_result")/informe.txt
 ## ejecutarlo desde aqui error?? no verificacion, sino que no encuentra la ruta, 
 
 
